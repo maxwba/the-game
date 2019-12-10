e1e3a3e6dec1a81671798f8d7f24f8818abe6e5c
Maxwell Martins Lima Monteiro <maxwba@AbelardosiPhone.localdomain>
2019-12-10 14:13:26 -0300

    Power ability implemented

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
-// Global varibles
+// Global variables
 let context, controller, player, loop, controller2, player2;
 let backgroundImg = new Image();
 let playerImg = new Image();
 let player2Img = new Image();
 let avatarPlayerImg = new Image();
 let avatarPlayer2Img = new Image();
+let energyStore = [];
+let frames = 0;
 
 // Screen sizes
 context = document.querySelector("canvas").getContext("2d");
@@ -14,7 +16,7 @@ let areaLeftX = 190;
 let areaRightX = 700;
 let areaDownY = 335;
 
-//Players
+// Player 1 object
 player = {
   height: 40,
   jumping: true,
@@ -26,6 +28,7 @@ player = {
   y_velocity: 0
 };
 
+// Player 2 object
 player2 = {
   height: 45,
   jumping: true,
@@ -37,13 +40,35 @@ player2 = {
   y_velocity: 0
 };
 
+// Power
+class EnergyBall {
+  constructor(x,y) {
+    this.x = x + 40; 
+    this.y = y + 10;
+  };
+
+  move() {
+    this.x += 1;
+  };
+
+  draw() {
+    context.fillStyle = 'Blue'
+    context.fillRect(this.x,this.y,20,20)
+  };
+
+  update() {
+    this.x = player.x + 40
+    this.y = player.y + 10
+  }
+};
+
 //Controler P1
 controller = {
   left: false,
   right: false,
   up: false,
   keyListener: function(event) {
-    let key_state = event.type == "keydown" ? true : false;
+    let key_state = event.type === "keydown";
 
     switch (event.keyCode) {
       case 65: // left key
@@ -55,7 +80,7 @@ controller = {
       case 68: // right key
         controller.right = key_state;
         break;
-      case 82: //space key
+      case 82: //R key
         controller.power = key_state;
     }
   }
@@ -85,122 +110,140 @@ controller2 = {
 };
 
 loop = function() {
-  if (controller.up && player.jumping == false) {
-    player.y_velocity -= 30;
-    player.jumping = true;
-  }
+  //Loop player 1 control
+  let playerControl = () => {
+    if (controller.up && player.jumping == false) {
+      player.y_velocity -= 30;
+      player.jumping = true;
+    }
 
-  if (controller.left) {
-    player.x_velocity -= 0.5;
-  }
+    if (controller.left) {
+      player.x_velocity -= 0.5;
+    }
 
-  if (controller.right) {
-    player.x_velocity += 0.5;
-  }
+    if (controller.right) {
+      player.x_velocity += 0.5;
+    }
 
-  player.y_velocity += 1.5; // gravity
-  player.x += player.x_velocity;
-  player.y += player.y_velocity;
-  player.x_velocity *= 0.9; // friction
-  player.y_velocity *= 0.9; // friction
+    player.y_velocity += 1.5; // gravity
+    player.x += player.x_velocity;
+    player.y += player.y_velocity;
+    player.x_velocity *= 0.9; // friction
+    player.y_velocity *= 0.9; // friction
 
-  // if player 1 is falling below floor line
-  if (player.y > areaDownY) {
-    player.jumping = false;
-    player.y = areaDownY;
-    player.y_velocity = 0;
-  }
+    // if player 1 is falling below floor line
+    if (player.y > areaDownY) {
+      player.jumping = false;
+      player.y = areaDownY;
+      player.y_velocity = 0;
+    }
 
-  // if player 1 touch the left of the screen
-  if (player.x < areaLeftX) {
-    player.x = areaLeftX;
-  } else if (player.x > areaRightX) {
-    // if player touch the right screen
-    player.x = areaRightX;
-  }
+    // if player 1 touch the left of the screen
+    if (player.x < areaLeftX) {
+      player.x = areaLeftX;
+    } else if (player.x > areaRightX) {
+      // if player touch the right screen
+      player.x = areaRightX;
+    }
+    // Player 1 colison
+    //   if (Math.floor(player.x) < Math.floor(player2.x + player2.width)&& player.y === player2.y) {
+    //       player.x += player2.width * 0.30
+    //       player2.x -= player.width * 0.30
+    //   }
+  };
+  playerControl();
 
-  //   // Player 1 colison
-  //   if (Math.floor(player.x) < Math.floor(player2.x + player2.width)&& player.y === player2.y) {
-  //       player.x += player2.width * 0.30
-  //       player2.x -= player.width * 0.30
-  //   }
+  //Loop player 2 control
+  let player2Control = () => {
+    if (controller2.up && player2.jumping == false) {
+      player2.y_velocity -= 30;
+      player2.jumping = true;
+    }
 
-  //P2
-  if (controller2.up && player2.jumping == false) {
-    player2.y_velocity -= 30;
-    player2.jumping = true;
-  }
+    if (controller2.left) {
+      player2.x_velocity -= 0.5;
+    }
 
-  if (controller2.left) {
-    player2.x_velocity -= 0.5;
-  }
+    if (controller2.right) {
+      player2.x_velocity += 0.5;
+    }
 
-  if (controller2.right) {
-    player2.x_velocity += 0.5;
-  }
+    player2.y_velocity += 1.5; // gravity
+    player2.x += player2.x_velocity;
+    player2.y += player2.y_velocity;
+    player2.x_velocity *= 0.9; // friction
+    player2.y_velocity *= 0.9; // friction
 
-  player2.y_velocity += 1.5; // gravity
-  player2.x += player2.x_velocity;
-  player2.y += player2.y_velocity;
-  player2.x_velocity *= 0.9; // friction
-  player2.y_velocity *= 0.9; // friction
+    // if player is falling below floor line
+    if (player2.y > areaDownY) {
+      player2.jumping = false;
+      player2.y = areaDownY;
+      player2.y_velocity = 0;
+    }
 
-  // if player is falling below floor line
-  if (player2.y > areaDownY) {
-    player2.jumping = false;
-    player2.y = areaDownY;
-    player2.y_velocity = 0;
-  }
-
-  // if player 2 touch the left of the screen
-  if (player2.x < areaLeftX) {
-    player2.x = areaLeftX;
-  } else if (player2.x > areaRightX) {
-    // if player 2 touch the right screen
-    player2.x = areaRightX;
-  }
+    // if player 2 touch the left of the screen
+    if (player2.x < areaLeftX) {
+      player2.x = areaLeftX;
+    } else if (player2.x > areaRightX) {
+      // if player 2 touch the right screen
+      player2.x = areaRightX;
+    }
+  };
+  player2Control();
 
   //Map
-  backgroundImg.src = "../img/background.png";
-  context.drawImage(backgroundImg, 0, 0, 1000, 500);
-  context.font = "15px Lucida Console";
-  context.fillStyle = "black";
-  context.fillText(`Goku health: ${player.health}`, 195, 110);
-  context.fillText(`Player 2 health: ${player2.health}`, 670, 110);
-  avatarPlayerImg.src = "../img/Goku_Avatar.png";
-  context.drawImage(avatarPlayerImg, 220, 120, 50, 50);
-  avatarPlayer2Img.src = "../img/Frezza_Avatar.png";
-  context.drawImage(avatarPlayer2Img, 710, 120, 50, 50);
-
-  //Player 1 power
-  if (controller.power) {
-    // Power position player
-    let powerX = player.x + 40;
-    let powerY = player.y + 10;
-    context.fillStyle = "blue";
-    context.fillRect(powerX, powerY, 20, 20);
-    for (let i = 0; i < areaRightX - powerX; i++) {
-      // context.clearRect(0, 0, 1000, 500);
-      context.clearRect(powerX, powerY, 20, 20);
-      context.fillRect(powerX, powerY, 20, 20);
-      powerX += 1;
-    }
-  }
+  let map = () => {
+    backgroundImg.src = "../img/background.png";
+    context.drawImage(backgroundImg, 0, 0, 1000, 500);
+    context.font = "15px Lucida Console";
+    context.fillStyle = "black";
+    context.fillText(`Goku health: ${player.health}`, 195, 110);
+    context.fillText(`Player 2 health: ${player2.health}`, 670, 110);
+    avatarPlayerImg.src = "../img/Goku_Avatar.png";
+    context.drawImage(avatarPlayerImg, 220, 120, 50, 50);
+    avatarPlayer2Img.src = "../img/Frezza_Avatar.png";
+    context.drawImage(avatarPlayer2Img, 710, 120, 50, 50);
+  };
+  map();
 
   //Player 1
-  playerImg.src = "../img/Goku.png";
-  context.drawImage(playerImg, player.x, player.y, player.width, player.height);
+  let drawPlayer1 = () => {
+    playerImg.src = "../img/Goku.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  };
+  drawPlayer1();
 
   //Player 2
-  player2Img.src = "../img/Frezza.png";
-  context.drawImage(
-    player2Img,
-    player2.x,
-    player2.y,
-    player2.width,
-    player2.height
-  );
+  let drawPlayer2 = () => {
+    player2Img.src = "../img/Frezza.png";
+    context.drawImage(
+      player2Img,
+      player2.x,
+      player2.y,
+      player2.width,
+      player2.height
+    );
+  };
+  drawPlayer2();
 
+  //Player power
+  if (controller.power) {
+    if (frames % 5 === 0) {
+      energyStore.push(new EnergyBall(player.x,player.y))
+    }
+  }
+  
+  energyStore.forEach(element => {
+    element.draw()
+    element.move()
+  });
+  frames += 1;
   // call update when the browser is ready to draw again
   window.requestAnimationFrame(loop);
 };
