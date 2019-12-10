e96950e5e77eac864d754bb46ad2b5fa4d49bbf0
Maxwell Martins Lima Monteiro <maxwba@AbelardosiPhone.localdomain>
2019-12-10 11:40:47 -0300

    Power WIP

--- src/script.js
+++ src/script.js
@@ -1,27 +1,35 @@
+// Global varibles
 let context, controller, player, loop, controller2, player2;
 let backgroundImg = new Image();
+let playerImg = new Image();
+let player2Img = new Image();
+let avatarPlayerImg = new Image();
+let avatarPlayer2Img = new Image();
 
+// Screen sizes
 context = document.querySelector("canvas").getContext("2d");
-
 context.canvas.height = 500;
 context.canvas.width = 1000;
+let areaLeftX = 190;
+let areaRightX = 700;
+let areaDownY = 335;
 
 //Players
 player = {
-  height: 32,
+  height: 40,
   jumping: true,
-  width: 32,
+  width: 40,
   health: 100,
-  x: 190, // center of the canvas
+  x: 190,
   x_velocity: 0,
   y: 0,
   y_velocity: 0
 };
 
 player2 = {
-  height: 32,
+  height: 45,
   jumping: true,
-  width: 32,
+  width: 45,
   health: 100,
   x: 700,
   x_velocity: 0,
@@ -39,14 +47,16 @@ controller = {
 
     switch (event.keyCode) {
       case 65: // left key
-        controller2.left = key_state;
+        controller.left = key_state;
         break;
       case 87: // up key
-        controller2.up = key_state;
+        controller.up = key_state;
         break;
       case 68: // right key
-        controller2.right = key_state;
+        controller.right = key_state;
         break;
+      case 82: //space key
+        controller.power = key_state;
     }
   }
 };
@@ -56,18 +66,19 @@ controller2 = {
   left: false,
   right: false,
   up: false,
+  power: false,
   keyListener: function(event) {
     let key_state = event.type == "keydown" ? true : false;
 
     switch (event.keyCode) {
       case 37: // left key
-        controller.left = key_state;
+        controller2.left = key_state;
         break;
       case 38: // up key
-        controller.up = key_state;
+        controller2.up = key_state;
         break;
       case 39: // right key
-        controller.right = key_state;
+        controller2.right = key_state;
         break;
     }
   }
@@ -75,7 +86,7 @@ controller2 = {
 
 loop = function() {
   if (controller.up && player.jumping == false) {
-    player.y_velocity -= 45;
+    player.y_velocity -= 30;
     player.jumping = true;
   }
 
@@ -94,18 +105,18 @@ loop = function() {
   player.y_velocity *= 0.9; // friction
 
   // if player 1 is falling below floor line
-  if (player.y > 350 - 16 - 32) {
+  if (player.y > areaDownY) {
     player.jumping = false;
-    player.y = 390 - 16 - 32;
+    player.y = areaDownY;
     player.y_velocity = 0;
   }
 
   // if player 1 touch the left of the screen
-  if (player.x < 190) {
-    player.x = 190;
-  } else if (player.x > 700) {
+  if (player.x < areaLeftX) {
+    player.x = areaLeftX;
+  } else if (player.x > areaRightX) {
     // if player touch the right screen
-    player.x = 700;
+    player.x = areaRightX;
   }
 
   //   // Player 1 colison
@@ -116,7 +127,7 @@ loop = function() {
 
   //P2
   if (controller2.up && player2.jumping == false) {
-    player2.y_velocity -= 45;
+    player2.y_velocity -= 30;
     player2.jumping = true;
   }
 
@@ -135,43 +146,60 @@ loop = function() {
   player2.y_velocity *= 0.9; // friction
 
   // if player is falling below floor line
-  if (player2.y > 350 - 16 - 32) {
+  if (player2.y > areaDownY) {
     player2.jumping = false;
-    player2.y = 390 - 16 - 32;
+    player2.y = areaDownY;
     player2.y_velocity = 0;
   }
 
   // if player 2 touch the left of the screen
-  if (player2.x < 190) {
-    player2.x = 190;
-  } else if (player2.x > 700) {
+  if (player2.x < areaLeftX) {
+    player2.x = areaLeftX;
+  } else if (player2.x > areaRightX) {
     // if player 2 touch the right screen
-    player2.x = 700;
+    player2.x = areaRightX;
   }
 
   //Map
-//   context.fillStyle = "#202020";
-  backgroundImg.src = "../img/background.jpeg"
-  context.drawImage(backgroundImg, 0, 0,1000,500);
-  
-//   context.fillRect(0, 0, 1000, 500); // x, y, width, height
-  context.font = "25px Lucida Console";
+  backgroundImg.src = "../img/background.png";
+  context.drawImage(backgroundImg, 0, 0, 1000, 500);
+  context.font = "15px Lucida Console";
   context.fillStyle = "black";
-  context.fillText(`Player 1 health: ${player.health}`, 10, 50);
-  context.fillText(`Player 2 health: ${player2.health}`, 750, 50);
+  context.fillText(`Goku health: ${player.health}`, 195, 110);
+  context.fillText(`Player 2 health: ${player2.health}`, 670, 110);
+  avatarPlayerImg.src = "../img/Goku_Avatar.png";
+  context.drawImage(avatarPlayerImg, 220, 120, 50, 50);
+  avatarPlayer2Img.src = "../img/Frezza_Avatar.png";
+  context.drawImage(avatarPlayer2Img, 710, 120, 50, 50);
 
+  //Player 1 power
+  if (controller.power) {
+    // Power position player
+    let powerX = player.x + 40;
+    let powerY = player.y + 10;
+    context.fillStyle = "blue";
+    context.fillRect(powerX, powerY, 20, 20);
+    for (let i = 0; i < areaRightX - powerX; i++) {
+      // context.clearRect(0, 0, 1000, 500);
+      context.clearRect(powerX, powerY, 20, 20);
+      context.fillRect(powerX, powerY, 20, 20);
+      powerX += 1;
+    }
+  }
 
   //Player 1
-  context.fillStyle = "#ff0000"; // Player 1
-  context.beginPath();
-  context.rect(player.x, player.y, player.width, player.height);
-  context.fill();
+  playerImg.src = "../img/Goku.png";
+  context.drawImage(playerImg, player.x, player.y, player.width, player.height);
 
   //Player 2
-  context.fillStyle = "yellow";
-  context.beginPath();
-  context.rect(player2.x, player2.y, player2.width, player2.height);
-  context.fill();
+  player2Img.src = "../img/Frezza.png";
+  context.drawImage(
+    player2Img,
+    player2.x,
+    player2.y,
+    player2.width,
+    player2.height
+  );
 
   // call update when the browser is ready to draw again
   window.requestAnimationFrame(loop);
