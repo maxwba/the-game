cf01db1b0fccf4ac9a13560e96b10aa43b7a8fde
Maxwell Martins Lima Monteiro <maxwba@AbelardosiPhone.localdomain>
2019-12-11 14:07:07 -0300

    Power img add

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ let playerImg = new Image();
 let player2Img = new Image();
 let avatarPlayerImg = new Image();
 let avatarPlayer2Img = new Image();
+let powerImg = new Image();
 let energyStoreGoku = [];
 let energyStoreFrezza = [];
 let frames = 0;
@@ -59,15 +60,21 @@ class EnergyBall {
 
   move() {
     if (this.side === "left") {
-      this.x += 1;
+      this.x += 7.5;
     } else {
-      this.x -= 1;
+      this.x -= 7.5;
     }
   }
 
   draw() {
-    context.fillStyle = "Blue";
-    context.fillRect(this.x, this.y, 20, 20);
+    powerImg.src = "../img/powerBall.png"
+    context.drawImage(
+      powerImg,
+      this.x,
+      this.y,
+      25,
+      25,
+    )
   }
 
   update() {
@@ -126,197 +133,20 @@ controller2 = {
 };
 
 loop = function() {
-  //Loop player 1 control
-  let playerControl = () => {
-    if (controller.up && player.jumping == false) {
-      player.y_velocity -= 30;
-      player.jumping = true;
-    }
 
-    if (controller.left) {
-      player.x_velocity -= 0.5;
-    }
-
-    if (controller.right) {
-      player.x_velocity += 0.5;
-    }
-
-    player.y_velocity += 1.5; // gravity
-    player.x += player.x_velocity;
-    player.y += player.y_velocity;
-    player.x_velocity *= 0.9; // friction
-    player.y_velocity *= 0.9; // friction
-
-    // if player 1 is falling below floor line
-    if (player.y > areaDownY) {
-      player.jumping = false;
-      player.y = areaDownY;
-      player.y_velocity = 0;
-    }
-
-    // if player 1 touch the left of the screen
-    if (player.x < areaLeftX) {
-      player.x = areaLeftX;
-    } else if (player.x > areaRightX) {
-      // if player touch the right screen
-      player.x = areaRightX;
-    }
-    // Player 1 colison
-    //   if (Math.floor(player.x) < Math.floor(player2.x + player2.width)&& player.y === player2.y) {
-    //       player.x += player2.width * 0.30
-    //       player2.x -= player.width * 0.30
-    //   }
-  };
   playerControl();
 
-  //Loop player 2 control
-  let player2Control = () => {
-    if (controller2.up && player2.jumping == false) {
-      player2.y_velocity -= 30;
-      player2.jumping = true;
-    }
-
-    if (controller2.left) {
-      player2.x_velocity -= 0.5;
-    }
-
-    if (controller2.right) {
-      player2.x_velocity += 0.5;
-    }
-
-    player2.y_velocity += 1.5; // gravity
-    player2.x += player2.x_velocity;
-    player2.y += player2.y_velocity;
-    player2.x_velocity *= 0.9; // friction
-    player2.y_velocity *= 0.9; // friction
-
-    // if player is falling below floor line
-    if (player2.y > areaDownY) {
-      player2.jumping = false;
-      player2.y = areaDownY;
-      player2.y_velocity = 0;
-    }
-
-    // if player 2 touch the left of the screen
-    if (player2.x < areaLeftX) {
-      player2.x = areaLeftX;
-    } else if (player2.x > areaRightX) {
-      // if player 2 touch the right screen
-      player2.x = areaRightX;
-    }
-  };
   player2Control();
 
-  //Map
-  let map = () => {
-    backgroundImg.src = "../img/background.png";
-    context.drawImage(backgroundImg, 0, 0, 1000, 500);
-    context.font = "15px Lucida Console";
-    context.fillStyle = "black";
-    context.fillText(`Goku health: ${player.health}`, 195, 110);
-    context.fillText(`Player 2 health: ${player2.health}`, 670, 110);
-    avatarPlayerImg.src = "../img/Goku_Avatar.png";
-    context.drawImage(avatarPlayerImg, 220, 120, 50, 50);
-    avatarPlayer2Img.src = "../img/Frezza_Avatar.png";
-    context.drawImage(avatarPlayer2Img, 710, 120, 50, 50);
-  };
   map();
 
-  //Player 1
-  let drawPlayer1 = () => {
-    if (player.x < player2.x) {
-      playerImg.src = "../img/Goku.png";
-      context.drawImage(
-        playerImg,
-        player.x,
-        player.y,
-        player.width,
-        player.height
-      );
-    } else {
-      playerImg.src = "../img/Goku_revert.png";
-      context.drawImage(
-        playerImg,
-        player.x,
-        player.y,
-        player.width,
-        player.height
-      );
-    }
-
-    if (controller.power && player.x < player2.x) {
-      playerImg.src = "../img/Goku_power-right.png";
-      context.drawImage(
-        playerImg,
-        player.x,
-        player.y,
-        player.width,
-        player.height
-      );
-    }
-
-    if (controller.power && player.x > player2.x) {
-      playerImg.src = "../img/Goku_power-left.png";
-      context.drawImage(
-        playerImg,
-        player.x,
-        player.y,
-        player.width,
-        player.height
-      );
-    }
-
-    if (controller.right && player.x < player2.x) {
-      playerImg.src = "../img/Goku_right.png";
-      context.drawImage(
-        playerImg,
-        player.x,
-        player.y,
-        player.width,
-        player.height
-      );
-    }
-
-    if (controller.left && player.x < player2.x) {
-      playerImg.src = "../img/Goku_left.png";
-      context.drawImage(
-        playerImg,
-        player.x,
-        player.y,
-        player.width,
-        player.height
-      );
-    }
-  };
   drawPlayer1();
 
-  //Player 2
-  let drawPlayer2 = () => {
-    if (player2.x > player.x) {
-      player2Img.src = "../img/Frezza.png";
-      context.drawImage(
-        player2Img,
-        player2.x,
-        player2.y,
-        player2.width,
-        player2.height
-      );
-    } else {
-      player2Img.src = "../img/Frezza_reverse.png";
-      context.drawImage(
-        player2Img,
-        player2.x,
-        player2.y,
-        player2.width,
-        player2.height
-      );
-    }
-  };
   drawPlayer2();
 
   //Player 1 power
   if (controller.power) {
-    if (frames % 5 === 0) {
+    if (frames % 30 === 0) {
       energyStoreGoku.push(new EnergyBall(player.x, player.y, player2.x));
       gokuAttack = true;
     }
@@ -324,7 +154,7 @@ loop = function() {
 
   // Player 2 power
   if (controller2.power) {
-    if (frames % 5 === 0) {
+    if (frames % 30 === 0) {
       energyStoreFrezza.push(new EnergyBall(player2.x, player2.y, player.x));
       frezzaAtack = true;
     }
@@ -381,6 +211,189 @@ loop = function() {
 
   // call update when the browser is ready to draw again
   window.requestAnimationFrame(loop);
+};
+
+//Loop player 1 control
+let playerControl = () => {
+  if (controller.up && player.jumping == false) {
+    player.y_velocity -= 30;
+    player.jumping = true;
+  }
+
+  if (controller.left) {
+    player.x_velocity -= 0.5;
+  }
+
+  if (controller.right) {
+    player.x_velocity += 0.5;
+  }
+
+  player.y_velocity += 1.5; // gravity
+  player.x += player.x_velocity;
+  player.y += player.y_velocity;
+  player.x_velocity *= 0.9; // friction
+  player.y_velocity *= 0.9; // friction
+
+  // if player 1 is falling below floor line
+  if (player.y > areaDownY) {
+    player.jumping = false;
+    player.y = areaDownY;
+    player.y_velocity = 0;
+  }
+
+  // if player 1 touch the left of the screen
+  if (player.x < areaLeftX) {
+    player.x = areaLeftX;
+  } else if (player.x > areaRightX) {
+    // if player touch the right screen
+    player.x = areaRightX;
+  }
+  // Player 1 colison
+  //   if (Math.floor(player.x) < Math.floor(player2.x + player2.width)&& player.y === player2.y) {
+  //       player.x += player2.width * 0.30
+  //       player2.x -= player.width * 0.30
+  //   }
+};
+
+//Loop player 2 control
+let player2Control = () => {
+  if (controller2.up && player2.jumping == false) {
+    player2.y_velocity -= 30;
+    player2.jumping = true;
+  }
+
+  if (controller2.left) {
+    player2.x_velocity -= 0.5;
+  }
+
+  if (controller2.right) {
+    player2.x_velocity += 0.5;
+  }
+
+  player2.y_velocity += 1.5; // gravity
+  player2.x += player2.x_velocity;
+  player2.y += player2.y_velocity;
+  player2.x_velocity *= 0.9; // friction
+  player2.y_velocity *= 0.9; // friction
+
+  // if player is falling below floor line
+  if (player2.y > areaDownY) {
+    player2.jumping = false;
+    player2.y = areaDownY;
+    player2.y_velocity = 0;
+  }
+
+  // if player 2 touch the left of the screen
+  if (player2.x < areaLeftX) {
+    player2.x = areaLeftX;
+  } else if (player2.x > areaRightX) {
+    // if player 2 touch the right screen
+    player2.x = areaRightX;
+  }
+};
+
+//Map
+let map = () => {
+  backgroundImg.src = "../img/background.png";
+  context.drawImage(backgroundImg, 0, 0, 1000, 500);
+  context.font = "15px Lucida Console";
+  context.fillStyle = "black";
+  context.fillText(`Goku health: ${player.health}`, 195, 110);
+  context.fillText(`Player 2 health: ${player2.health}`, 670, 110);
+  avatarPlayerImg.src = "../img/Goku_Avatar.png";
+  context.drawImage(avatarPlayerImg, 220, 120, 50, 50);
+  avatarPlayer2Img.src = "../img/Frezza_Avatar.png";
+  context.drawImage(avatarPlayer2Img, 710, 120, 50, 50);
+};
+
+//Player 1
+let drawPlayer1 = () => {
+  if (player.x < player2.x) {
+    playerImg.src = "../img/Goku.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  } else {
+    playerImg.src = "../img/Goku_revert.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  }
+
+  if (controller.power && player.x < player2.x) {
+    playerImg.src = "../img/Goku_power-right.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  }
+
+  if (controller.power && player.x > player2.x) {
+    playerImg.src = "../img/Goku_power-left.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  }
+
+  if (controller.right && player.x < player2.x) {
+    playerImg.src = "../img/Goku_right.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  }
+
+  if (controller.left && player.x < player2.x) {
+    playerImg.src = "../img/Goku_left.png";
+    context.drawImage(
+      playerImg,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+  }
+};
+
+//Player 2
+let drawPlayer2 = () => {
+    if (player2.x > player.x) {
+      player2Img.src = "../img/Frezza.png";
+      context.drawImage(
+        player2Img,
+        player2.x,
+        player2.y,
+        player2.width,
+        player2.height
+      );
+    } else {
+      player2Img.src = "../img/Frezza_reverse.png";
+      context.drawImage(
+        player2Img,
+        player2.x,
+        player2.y,
+        player2.width,
+        player2.height
+      );
+    }
 };
 
 window.addEventListener("keydown", controller.keyListener);
